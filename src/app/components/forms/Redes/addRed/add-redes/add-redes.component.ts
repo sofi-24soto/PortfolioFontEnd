015d4b8494a3515127e63252f8691f60c3e2f445
Sofii283soto <sofiasoto283@gmail.com>
2023-05-12 08:23:08 -0300

    se agrego validacion en los campos del formulario add-redes

--- src/app/components/forms/Redes/addRed/add-redes/add-redes.component.ts
+++ src/app/components/forms/Redes/addRed/add-redes/add-redes.component.ts
@@ -10,21 +10,34 @@ import { RedesService } from 'src/app/servicios/redes.service';
   styleUrls: ['./add-redes.component.css']
 })
 export class AddRedesComponent implements OnInit {
+  form: FormGroup;
   url_redes : string;
   icoFont : string;
 
   constructor(
+    private formBuilder: FormBuilder,
     private redesService: RedesService,
     private router : Router
     
-  ){}
-  ngOnInit(): void {
-    
+  ){
+
+    this.form= this.formBuilder.group({
+      url_redes:['',[Validators.required]],  
+      icoFont:['', [Validators.required]],  
+    })
   }
 
-  
+  ngOnInit(): void {}
 
-  crearRed(): void{
+  get Link(){
+    return this.form.get("url_redes");
+  }
+
+  get Icono(){
+    return this.form.get("icoFont");
+  }
+
+  onCreate(): void{
     const redSocial = new Redes(this.url_redes, this.icoFont);
     this.redesService.create(redSocial).subscribe(
       data => {
@@ -37,9 +50,23 @@ export class AddRedesComponent implements OnInit {
     )
   }
 
+  onEnviar(event:Event){
+    event.preventDefault;
+    if (this.form.valid){
+      this.onCreate();
+      this.router.navigate(['']);
+    }else{
+      alert("falló en la carga, intente nuevamente");
+      this.form.markAllAsTouched();
+    }
+  }
+
+
+
+
+
   limpiar(): void{
-    this.url_redes = "";
-    this.icoFont = "";
+   this.form.reset();
   }
 
   
