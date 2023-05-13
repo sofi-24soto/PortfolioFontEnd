73cdcc9a9a24b4d1e31b16688664a169590a4d28
Sofii283soto <sofiasoto283@gmail.com>
2023-05-13 11:04:21 -0300

    últimos cambios en los formularios de los edits

--- src/app/components/forms/Habilidad/EditarHabilidad/edit-habilidad/edit-habilidad.component.ts
+++ src/app/components/forms/Habilidad/EditarHabilidad/edit-habilidad/edit-habilidad.component.ts
@@ -10,20 +10,16 @@ import { HabilidadService } from 'src/app/servicios/habilidad.service';
   styleUrls: ['./edit-habilidad.component.css']
 })
 export class EditHabilidadComponent implements OnInit{
-  form: FormGroup;
-  habilidad: Habilidad;
-
+ 
+  habilidad: Habilidad = null;
+  porcentaje: number = 0;
   constructor(
-    private formBuilder: FormBuilder, 
+   
     private habilidadService: HabilidadService,
     private activatedRouter: ActivatedRoute,
     private router: Router) {
 
-      this.form= this.formBuilder.group({
-        nombre:['',[Validators.required]],  
-        porcentaje:['', [Validators.required, Validators.min(0), Validators.max(100)]],
-          
-      })
+     
      }
 
   ngOnInit(): void {
@@ -31,42 +27,21 @@ export class EditHabilidadComponent implements OnInit{
     this.habilidadService.ver(id).subscribe(
       data => {
         this.habilidad = data;
-      }, err => {
-        alert("Error...!! vuelva a intentarlo nuevamente");
-        this.router.navigate(['']);
-      }
-    )
+      })
+
   }
 
-  get Habilidad(){
-    return this.form.get("nombre");
-  }
-  
-  get Porcentaje(){
-    return this.form.get("porcentaje");
-  }
-
- onUpdate(): void{
+  editarHabilidad():void{
     const id = this.activatedRouter.snapshot.params['id'];
-    this.habilidadService.update(id,this.form.value).subscribe(
+    this.habilidadService.update(id,this.habilidad).subscribe(
       data => {
         this.router.navigate(['']);
-      }, err => {
-        alert("Error...!! vuelva a intentarlo nuevamente");
+      }, err =>{
+        //alert("Error al modificar los datos de habilidad");
         this.router.navigate(['']);
       }
     )
   }
 
-  onEnviar(event:Event){
-    event.preventDefault;
-    if (this.form.valid){
-      this.onUpdate();
-      this.router.navigate(['']);
-    }else{
-      alert("falló en la carga, intente nuevamente");
-      this.form.markAllAsTouched();
-    }
-  }
 
 }
